fcf74b4c803dec7d9fd05064d84eeed25561deca
Dom7k <dominicnaatey@gmail.com>
2022-12-17 16:11:45 +0000

    Openai Library Request/Response

--- controllers/openaiControllers.js
+++ controllers/openaiControllers.js
@@ -1,7 +1,38 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const generateimage = async (req, res) => {
-    res.status(200).json({
-        success: true,
-    });
+    try {
+        const response = await openai.createImage({
+            prompt: "aliens in a cenima watching spiderman",
+            n: 1,
+            size: "1024x1024",
+          });
+
+        const imageUrl = response.data.data[0].url;
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl,
+          });
+
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
+
+        res.status(400).json({
+            success: false,
+            error: 'The image could not be generated'
+          });
+    }
 };
 
 module.exports = {generateimage}
